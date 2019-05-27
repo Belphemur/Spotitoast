4cd3a06c7100b2c445dc8279c7339e6689ef6014
Antoine Aflalo <antoine+github@aaflalo.me>
2019-05-27 22:47:37 +0100

    Add demo project

---
+++ Spotitoast.Demo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spotitoast.Banner.Client;
+using Spotitoast.Spotify.Client;
+
+namespace Spotitoast
+{
+    public partial class Form1 : Form
+    {
+        private delegate void SetString(string str);
+
+        private SpotifyClient _client;
+        public Form1(SpotifyClient spotifyClient)
+        {
+            _client = spotifyClient;
+            BannerClient.Setup();
+            InitializeComponent();
+        }
+
+        public void UpdateTrackLabel(string label)
+        {
+            BeginInvoke(new SetString(SetTrackLabel), label);
+
+        }
+
+        private void SetTrackLabel(string label)
+        {
+            this.trackLabel.Text = label;
+        }
+
+        private void LoveButton_Click(object sender, EventArgs e)
+        {
+            _client.LikePlayedTrack();
+        }
+
+        private void UnloveButton_Click(object sender, EventArgs e)
+        {
+            _client.DislikePlayedTrack();
+        }
+
+        private void PlayPauseButton_Click(object sender, EventArgs e)
+        {
+            if (_client.IsPlaying)
+            {
+                _client.Pause();
+                return;
+            }
+
+            _client.Resume();
+        }
+    }
+}
---
+++ Spotitoast.Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spotitoast.Banner.Client;
+using Spotitoast.Banner.Model;
+using Spotitoast.Configuration;
+using Spotitoast.Spotify.Client;
+
+namespace Spotitoast
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            var configManager = new ConfigurationManager(Path.Combine(folderPath, "Spotitoast"));
+
+            var client = new SpotifyClient(configManager);
+
+            var mainForm = new Form1(client);
+
+
+            client.CurrentlyPlayedTrackChanged += (sender, @event) =>
+            {
+                var trackName = @event.Track.Name;
+                var artists = string.Join(", ", @event.Track.Artists.Select(artist => artist.Name));
+                var imageUrl = @event.Track.Album.Images.First().Url;
+                mainForm.UpdateTrackLabel(trackName);
+                var bannerData = new BannerData(imageUrl, new Size(100, 100))
+                {
+                    Title = trackName,
+                    Text = @event.Track.Album.Name,
+                    SubText = artists
+                };
+
+                BannerClient.ShowNotification(bannerData);
+            };
+
+
+            Application.Run(mainForm);
+        }
+    }
+}
