0b5f59bace92dcd1c17467a3249fe87c43fd3094
Antoine Aflalo <antoine+github@aaflalo.me>
2019-05-29 12:42:28 -0400

    Add first version of the trayicon

---
+++ Spotitoast/Context/SpotitoastContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Spotitoast.Context
+{
+    public class SpotitoastContext : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+
+        public SpotitoastContext()
+        {
+            // Initialize Tray Icon
+            using var manifestResourceStream = Assembly.GetEntryAssembly()?.GetManifestResourceStream("Spotitoast.Resources.spotify.ico") ?? throw new InvalidOperationException();
+            _trayIcon = new NotifyIcon()
+            {
+                Icon = new Icon(manifestResourceStream),
+                ContextMenu = new ContextMenu(new[] {
+                    new MenuItem("Exit", Exit)
+                }),
+                BalloonTipTitle = "Spotitoast",
+                Text = "Spotitoast",
+                Visible = true
+            };
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            _trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
