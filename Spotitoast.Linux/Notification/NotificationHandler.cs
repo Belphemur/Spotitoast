56f8c1b8ab459b7884498ae9bce0215eaeb0b4dc
Antoine Aflalo <git@aaflalo.me>
2020-07-02 17:55:10 -0400

    Add action to track played

--- Spotitoast.Linux/Notification/NotificationHandler.cs
+++ Spotitoast.Linux/Notification/NotificationHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using Notify.Linux.Client;
+using Spotitoast.Linux.Command;
+using Spotitoast.Logic.Business.Action;
 using Spotitoast.Logic.Business.Player;
 using Spotitoast.Logic.Framework.Extensions;
 
@@ -8,13 +10,15 @@ namespace Spotitoast.Linux.Notification
 {
     public class NotificationHandler : INotificationHandler
     {
-        private readonly ISpotifyNotifier _spotifyClient;
+        private readonly ISpotifyNotifier _spotifyNotifier;
         private readonly INotificationClient _notificationClient;
+        private readonly ICommandExecutor _commandExecutor;
 
-        public NotificationHandler(ISpotifyNotifier spotifyClient, INotificationClient notificationClient)
+        public NotificationHandler(ISpotifyNotifier spotifyNotifier, INotificationClient notificationClient, ICommandExecutor commandExecutor)
         {
-            _spotifyClient = spotifyClient;
+            _spotifyNotifier = spotifyNotifier;
             _notificationClient = notificationClient;
+            _commandExecutor = commandExecutor;
         }
 
         /// <summary>
@@ -28,7 +32,7 @@ namespace Spotitoast.Linux.Notification
 
         private void RegisterLikedDisliked()
         {
-            _spotifyClient.TrackLiked.Subscribe(async trackTask =>
+            _spotifyNotifier.TrackLiked.Subscribe(async trackTask =>
             {
                 var track = await trackTask;
                 await _notificationClient.NotifyAsync(new SpotitoastNotification
@@ -41,7 +45,7 @@ namespace Spotitoast.Linux.Notification
 
             });
             
-            _spotifyClient.TrackDisliked.Subscribe(async trackTask =>
+            _spotifyNotifier.TrackDisliked.Subscribe(async trackTask =>
             {
                 var track = await trackTask;
                 await _notificationClient.NotifyAsync(new SpotitoastNotification
@@ -56,7 +60,7 @@ namespace Spotitoast.Linux.Notification
 
         private void RegisterTrackPlayed()
         {
-            _spotifyClient.TrackPlayed.Subscribe(async trackTask =>
+            _spotifyNotifier.TrackPlayed.Subscribe(async trackTask =>
             {
                 var track = await trackTask;
                 var notificationData = new SpotitoastNotification
@@ -66,6 +70,31 @@ namespace Spotitoast.Linux.Notification
                     Expiration = 1,
                     Image = (await track.Album.Art).ResizeImage(new Size(100, 100))
                 };
+                if (!track.IsLoved)
+                {
+                    notificationData.Actions = new[]
+                    {
+                        new NotificationData.Action
+                        {
+                            Key = ActionFactory.PlayerAction.Like.ToString(),
+                            Label = " 💖 Like",
+                            OnActionCalled = () => _commandExecutor.Execute(ActionFactory.PlayerAction.Like)
+                        },
+                    };
+                }
+                else
+                {
+                    notificationData.Actions = new[]
+                    {
+                        new NotificationData.Action
+                        {
+                            Key = ActionFactory.PlayerAction.Dislike.ToString(),
+                            Label = "  🖤Dislike",
+                            OnActionCalled = () => _commandExecutor.Execute(ActionFactory.PlayerAction.Dislike)
+                        },
+                    };
+                }
+
                 await _notificationClient.NotifyAsync(notificationData);
             });
         }
