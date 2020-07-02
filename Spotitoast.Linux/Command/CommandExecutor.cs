3f0a32fc36792570a4d01802fe7880ffeb0c9805
Antoine Aflalo <git@aaflalo.me>
2020-07-02 15:49:36 -0400

    Add command executor

---
+++ Spotitoast.Linux/Command/CommandExecutor.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Spotitoast.Logic.Business.Action;
+using Spotitoast.Spotify.Model;
+
+namespace Spotitoast.Linux.Command
+{
+    public class CommandExecutor : ICommandExecutor
+    {
+        private readonly IActionFactory _actionFactory;
+
+        public CommandExecutor(IActionFactory actionFactory)
+        {
+            _actionFactory = actionFactory;
+        }
+
+        /// <summary>
+        /// Parse the command
+        /// </summary>
+        public ActionFactory.PlayerAction? ParseCommand(string cmd)
+        {
+            if (!Enum.TryParse(typeof(ActionFactory.PlayerAction), cmd, true, out var @enum))
+            {
+                return null;
+            }
+
+            return (ActionFactory.PlayerAction) @enum!;
+        }
+
+        /// <summary>
+        /// Execute the command
+        /// </summary>
+        public Task<ActionResult> Execute(ActionFactory.PlayerAction action) => _actionFactory.Get(action).Execute();
+    }
+}
---
+++ Spotitoast.Linux/Command/ICommandExecutor.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Spotitoast.Logic.Business.Action;
+using Spotitoast.Spotify.Model;
+
+namespace Spotitoast.Linux.Command
+{
+    public interface ICommandExecutor
+    {
+        /// <summary>
+        /// Parse the command
+        /// </summary>
+        ActionFactory.PlayerAction? ParseCommand(string cmd);
+
+        /// <summary>
+        /// Execute the command
+        /// </summary>
+        Task<ActionResult> Execute(ActionFactory.PlayerAction action);
+    }
+}
