8714798090b78016f0e13e1336d2f28c21b7003c
Antoine Aflalo <git@aaflalo.me>
2020-07-02 11:59:24 -0400

    Remove unneeded module loading

--- Spotitoast.Logic/Dependencies/Bootstrap.cs
+++ Spotitoast.Logic/Dependencies/Bootstrap.cs
@@ -1,20 +1,9 @@
 ﻿using Ninject;
-using Ninject.Modules;
-using Spotitoast.Logic.Dependencies.Actions;
-using Spotitoast.Logic.Dependencies.Configuration;
-using Spotitoast.Logic.Dependencies.Spotify;
 
 namespace Spotitoast.Logic.Dependencies
 {
     public static class Bootstrap
     {
-        private static readonly INinjectModule[] _modules =
-        {
-            new ConfigurationModule(),
-            new ActionModule(),
-            new SpotifyModule(),
-        };
-
-        public static IKernel Kernel = new StandardKernel(_modules);
+        public static IKernel Kernel = new StandardKernel();
     }
 }
