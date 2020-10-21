774eee20ff4c0d71010c695ba0dae76cdc3d11b8
Antoine Aflalo <git@aaflalo.me>
2020-10-21 12:05:09 -0400

    Add job scheduler project

--- Job.Scheduler/Utils/TaskExtensions.cs
+++ Job.Scheduler/Utils/TaskExtensions.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Spotitoast.Spotify.Scheduler
+namespace Job.Scheduler.Utils
 {
     public static class TaskExtensions
     {
