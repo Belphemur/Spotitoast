3f039ee2ee7163217bdeb7ebfdbbdfe89201474d
Antoine Aflalo <git@aaflalo.me>
2020-12-07 13:38:25 -0500

    Refactor to be cleaner between server and client

--- Spotitoast.Linux/Program.cs
+++ Spotitoast.Linux/Program.cs
@@ -12,21 +12,36 @@ namespace Spotitoast.Linux
         static async Task Main(string[] args)
         {
             Logic.Dependencies.Bootstrap.Kernel.Load(AppDomain.CurrentDomain.GetAssemblies());
-            var pipeName = $"Spotitoast-{Environment.UserName}";
-            using var mutex = new Mutex(true, @$"Global\{pipeName}", out var createdNew);
+            var mutexName = $"Spotitoast-{Environment.UserName}";
+            using var mutex = new Mutex(true, @$"Global\{mutexName}", out var createdNew);
             //When creating the mutex, we run a server
             var port = 25255;
             if (createdNew)
             {
-                await Console.Out.WriteLineAsync("Running as server.");
-                await Logic.Dependencies.Bootstrap.Kernel.Get<ServerContext>().EventLoopStart(port);
-                await Logic.Dependencies.Bootstrap.Kernel.Get<IJobScheduler>().StopAsync();
+                await RunServer(port);
                 return;
             }
 
+            await SendClientCommand(args, port);
+        }
+
+        private static async Task SendClientCommand(string[] args, int port)
+        {
             using var clientContext = Logic.Dependencies.Bootstrap.Kernel.Get<ClientContext>();
             await clientContext.ConnectAsync(port);
             await clientContext.SendCommand(args);
+        }
+
+        private static async Task RunServer(int port)
+        {
+            Console.CancelKeyPress += async (sender, eventArgs) =>
+            {
+                await Logic.Dependencies.Bootstrap.Kernel.Get<IJobScheduler>().StopAsync();
+                Environment.Exit(0);
+            };
+            await Console.Out.WriteLineAsync("Running as server.");
+            await Logic.Dependencies.Bootstrap.Kernel.Get<ServerContext>().EventLoopStart(port);
+            await Logic.Dependencies.Bootstrap.Kernel.Get<IJobScheduler>().StopAsync();
         }
     }
 }
